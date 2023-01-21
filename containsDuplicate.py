582bef695046118d0d9c99ce89fa2193d7a04eba
ChrisIBill <115194443+ChrisIBill@users.noreply.github.com>
2023-01-21 07:06:17 -0600

    containsDuplicate

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,5 +1,37 @@
+import random
+from typing import List
+
+
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        while (num = nums.pop()):
-            
-        
+    # i = containsDuplicate([5, 6, 7])
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    # def gen_list(self, length):
+    #     for x in range(length):
+    #         print(x + " ", end='')
+    #     conta
+
+    def containsDuplicate(nums: List[int]) -> bool:
+        return False if len(set(nums)) == len(nums) else True
+
+
+def runner(length) -> bool:
+    myList = [random.randint(1, 10) for x in range(length)]
+    print(list(myList))
+    a = Solution
+    print(a.containsDuplicate(myList))
+
+
+def containsDuplicateOld(nums: List[int]) -> bool:
+    while True:
+        try:
+            num = nums.pop()
+            try:
+                nums.index(num)
+                return True
+            except ValueError:
+                continue
+        except IndexError:
+            return False
